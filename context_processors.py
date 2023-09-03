752fd9bf0c1d0897136892a5eb75b47ad2ec7a7a
tougshire <tougshire@tougshire.com>
2023-09-03 09:49:07 -0400

    Add code to return the active scheme

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 
 def tougcomsys(request):
+    #the following line adds the name of the active theme to be returned with the key name 'active'
+    settings.TOUGCOMSYS[settings.TOUGCOMSYS['active']]['active']=settings.TOUGCOMSYS['active']
     return {
         "tougcomsys": settings.TOUGCOMSYS[settings.TOUGCOMSYS['active']]
     }
