af44a79cb02d96a1d423f396d0359f728df5d603
tougshire <tougshire@tougshire.com>
2023-10-30 06:01:24 -0400

    add slash to url and remove unused imports

--- management/commands/fetch_icals.py
+++ management/commands/fetch_icals.py
@@ -1,9 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-from tougcomsys.models import ArticlePlacement
 from tougcomsys.tasks import (
     fetch_icals as fetch_icals_task,
 )
-from datetime import date
 
 
 class Command(BaseCommand):
